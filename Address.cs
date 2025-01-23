1796f8dbb251bce3e4e743876490ad1052b6fcdf
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-01-23 22:58:24 +0200

    Add files via upload

---
+++ Address.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_2
+{
+    /// <summary>
+    /// Класс "Адрес"
+    /// </summary>
+    internal class Address
+    {
+        public int index;
+
+        public string country;
+
+        public string city;
+
+        public string street;
+
+        public int house;
+
+        public int apartment;
+
+        public void Output()
+        {
+            Console.WriteLine($"Индекс: {index}\tСтрана: {country}\tГород: " +
+                $"{city}\tУлица: {street}\tДом: {house}\tКвартира: {apartment}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Address ttiipAddress = new Address();
+
+            ttiipAddress.index = 3300;
+            ttiipAddress.country = "Приднестровье";
+            ttiipAddress.city = "Тирасполь";
+            ttiipAddress.street = "Карла Либкнехта";
+            ttiipAddress.house = 244;
+            ttiipAddress.apartment = 4;
+
+            ttiipAddress.Output();
+
+            Console.ReadKey();
+        }
+    }
+}
